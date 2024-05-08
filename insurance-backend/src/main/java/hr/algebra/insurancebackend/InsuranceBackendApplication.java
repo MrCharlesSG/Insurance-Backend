b777c077d97b9ec322cc20954c9dd99f61f623eb
MrCharlesSG <cserranogut@gmail.com>
2024-05-08 17:10:55 +0200

    First Commit

---
+++ insurance-backend/src/main/java/hr/algebra/insurancebackend/InsuranceBackendApplication.java
@@ -0,0 +1,13 @@
+package hr.algebra.insurancebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceBackendApplication.class, args);
+	}
+
+}
---
+++ insurance-backend/src/test/java/hr/algebra/insurancebackend/InsuranceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.algebra.insurancebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
